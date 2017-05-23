a8d8d684c2dbaa84340b84951d12ff37038d8f4f
Artur Kawala <kawala.artek@gmail.com>
2017-05-23 14:24:51 +0200

    Ostateczna wersja

--- main.cpp
+++ main.cpp
@@ -22,8 +22,12 @@ int ini_flag=0,n_flag,func_flag,*SS;
 
 //Bent Cigar Functions from Basic Section
 //pomocnicza funkcja, na której będziemy testować
+double switch_basic(const int, double*, const int);
 double bent_cigar_basic(double*, const int);
 double sum_of_diff_pow(double*, const int);
+double zakharov_basic(double*, const int);
+double rosenbrock_basic(double*, const int);
+double rastrigin_basic(double*, const int);
 
 
 int main()
@@ -43,6 +47,7 @@ int main()
     //3 - zakharov
     //4 - rosenbrock
     //5 - rastrigin
+    /*
     int func_nums[4] = { 1, 3, 4, 5 };
 
 	for (i = 0; i < 4; i++)
@@ -80,7 +85,7 @@ int main()
             }
             printf("\n");
         }
-	}
+	}*/
 
 
 
@@ -90,12 +95,17 @@ int main()
     //=========================================================
     double GLOBAL_AVG = 0.0;
     double GLOBAL_BEST = DBL_MAX;
+    double RESULTS[30];
+    bool section_basic = true;
     int f_num = 5;
+//    int population = 10;
+//    int population = 20;
+//    int population = 30;
+    int population = 40;
+    int itMax = 100000 / population;
 
     //Initialize a population
     for (int iter = 0; iter < 30; iter++) {
-        int itMax = 5000;
-        int population = 20;
         Star **arrOfStars = new Star *[population];
 
         for (int i = 0; i < population; i++) {
@@ -110,11 +120,14 @@ int main()
             //obliczanie wartości funkcji
             for (int i = 0; i < population; i++) {
                 double *pos = arrOfStars[i]->GetPosition();
-//                double val = sum_of_diff_pow(pos, n);
-//                double val = bent_cigar_basic(pos, n);
-                double f[1];
-                cec17_test_func(pos, f, n, 1, f_num);
-                arrOfStars[i]->SetFunctionValue(f[0]);
+                if(section_basic) {
+                    double val = switch_basic(f_num, pos, n);
+                    arrOfStars[i]->SetFunctionValue(val);
+                } else {
+                    double f[1];
+                    cec17_test_func(pos, f, n, 1, f_num);
+                    arrOfStars[i]->SetFunctionValue(f[0]);
+                }
             }
 
             //szukanie najlepszej wartości
@@ -131,11 +144,12 @@ int main()
                     double *old_pos = arrOfStars[i]->GetPosition();
                     double *new_pos = new double[n];
                     double *black_hole_pos = Black_Hole->GetPosition();
-                    double _rand = rand() % 1001 / 1000.0;
                     for (int j = 0; j < n; j++) {
-                        new_pos[j] = old_pos[j] + _rand * (black_hole_pos[j] - old_pos[j]);
+                        double _rand = (double)(rand() % 1001) / 1000.0;
+                       new_pos[j] = old_pos[j] + _rand * (black_hole_pos[j] - old_pos[j]);
                     }
                     arrOfStars[i]->SetPosition(new_pos);
+
                 }
             }
 
@@ -144,11 +158,15 @@ int main()
                 //liczenie wartości
                 if (arrOfStars[i] != Black_Hole) {
                     double *pos = arrOfStars[i]->GetPosition();
-//                    double val = sum_of_diff_pow(pos, n);
-//                    double val = bent_cigar_basic(pos, n);
-                    double f[1];
-                    cec17_test_func(pos, f, n, 1, f_num);
-                    arrOfStars[i]->SetFunctionValue(f[0]);
+
+                    if(section_basic) {
+                        double val = switch_basic(f_num, pos, n);
+                        arrOfStars[i]->SetFunctionValue(val);
+                    } else {
+                        double f[1];
+                        cec17_test_func(pos, f, n, 1, f_num);
+                        arrOfStars[i]->SetFunctionValue(f[0]);
+                    }
 
                     //sprawdzenie, czy lepsze od czarnej dziury
                     double current_val = arrOfStars[i]->GetFunctionValue();
@@ -171,8 +189,8 @@ int main()
                 }
             }
             double R = nom / denom;
-//            R/=10.0;
-//            R /= 10.0;
+
+            // R /= 100.0;
             //  2. sprawdzenie
             for (int i = 0; i < population; i++) {
                 double distance = 0.0;
@@ -183,40 +201,59 @@ int main()
                 if (arrOfStars[i] != Black_Hole && distance < R) {
                     delete arrOfStars[i];
                     arrOfStars[i] = new Star(n);
+                    double f[1];
+                    double* current_pos = arrOfStars[i]->GetPosition();
+                    cec17_test_func(current_pos, f, n, 1, f_num);
+                    arrOfStars[i]->SetFunctionValue(f[0]);
+                    if (arrOfStars[i]->GetFunctionValue() < Black_Hole->GetFunctionValue()) {
+                        Black_Hole = arrOfStars[i];
+                    }
                 }
             }
-//            std::cout << local_best << std::endl;
         }
 
-        double global_best = DBL_MAX;
+        double global_best = Black_Hole->GetFunctionValue();
         //szukanie ostatni raz najlepszej wartości
         //najpierw ponowne ustawienie
         for (int i = 0; i < population; i++) {
             double *curr_pos = arrOfStars[i]->GetPosition();
-//            double curr_val = sum_of_diff_pow(curr_pos, n);
-//            double curr_val = bent_cigar_basic(curr_pos, n);
-            double f[1];
-            cec17_test_func(curr_pos, f, n, 1, f_num);
-            arrOfStars[i]->SetFunctionValue(f[0]);
+
+            if(section_basic) {
+                double val = switch_basic(f_num, curr_pos, n);
+                arrOfStars[i]->SetFunctionValue(val);
+            } else {
+                double f[1];
+                cec17_test_func(curr_pos, f, n, 1, f_num);
+                arrOfStars[i]->SetFunctionValue(f[0]);
+            }
         }
         for (int i = 0; i < population; i++) {
-//        std::cout << "in f: " << arrOfStars[i]->GetFunctionValue() << std::endl;
-            //printf("Pos: %.6lf\n", arrOfStars[i]->GetFunctionValue());
             if (arrOfStars[i]->GetFunctionValue() < global_best) {
                 global_best = arrOfStars[i]->GetFunctionValue();
             }
         }
 
-//        std::cout << "Result of algorithm: " << global_best << std::endl;
-//        GLOBAL_BEST = (global_best < GLOBAL_BEST) ? global_best : GLOBAL_BEST;
-        GLOBAL_AVG += global_best;
+        RESULTS[iter] = global_best;
         GLOBAL_BEST = (global_best < GLOBAL_BEST) ? global_best : GLOBAL_BEST;
+        GLOBAL_AVG += global_best;
         for(int i = 0; i < population; i++)
             delete arrOfStars[i];
         delete [] arrOfStars;
     }
-    std::cout << "Average bests: " << GLOBAL_AVG / 30.0 << std::endl;
-    std::cout << "Global best: " << GLOBAL_BEST << std::endl;
+
+    GLOBAL_AVG /= 30.0;
+
+    double std_dev = 0;
+    for(int i=0; i<30; i++) {
+        std_dev += pow(RESULTS[i] - GLOBAL_AVG, 2);
+    }
+    std_dev /= (double)population;
+    std_dev = sqrt(std_dev);
+
+    std::cout /*<< "Average bests: "*/ << GLOBAL_AVG << std::endl;
+    std::cout /*<< "Global best: "*/ << GLOBAL_BEST << std::endl;
+    std::cout /*<< "Standard deviation: "*/ << std_dev << std::endl;
+
     // Free memory
 	free(x);
 	free(f);
@@ -233,6 +270,23 @@ int main()
 
 //=========================================================
 
+double switch_basic(const int f_num, double* x, const int D) {
+    switch (f_num) {
+        case 1:
+            return bent_cigar_basic(x, D);
+        case 2:
+            return sum_of_diff_pow(x, D);
+        case 3:
+            return zakharov_basic(x, D);
+        case 4:
+            return rosenbrock_basic(x, D);
+        case 5:
+            return rastrigin_basic(x, D);
+        default:
+            return -1.0;
+    }
+}
+
 double bent_cigar_basic(double* x, const int D) {
     double sum = 0.0;
     for(int i = 2; i <= D; i++) {
@@ -248,6 +302,35 @@ double sum_of_diff_pow(double* x, const int D) {
     double sum = 0.0;
     for(int i = 1; i <= D; i++) {
         sum += pow(fabs(x[i - 1]), i + 1);
+    }
+    return sum;
+}
+
+double zakharov_basic(double* x, const int D) {
+    double sum = 0.0;
+    for(int i = 0; i < D; i++) {
+        sum += pow(x[i], 2);
+    }
+    double sum_tmp = 0.0;
+    for(int i = 0; i < D; i++) {
+        sum_tmp += x[i] * 0.5;
+    }
+    sum += pow(sum_tmp, 2) + pow(sum_tmp, 4);
+    return sum;
+}
+
+double rosenbrock_basic(double* x, const int D) {
+    double sum = 0.0;
+    for(int i = 0; i < D - 1; i++) {
+        sum += 100.0 * pow(pow(x[i], 2) - x[i+1], 2) + pow(x[i] - 1.0, 2);
+    }
+    return sum;
+}
+
+double rastrigin_basic(double* x, const int D) {
+    double sum = 0.0;
+    for(int i = 0; i < D; i++) {
+        sum += pow(x[i], 2) - 10.0 * cos(2 * M_PI * x[i]) + 10.0;
     }
     return sum;
 }
